e78c2481f03032e69ec80fcc1d8b7257f12a33fd
Olga Konstantinova <kola43843@gmail.com>
2025-03-01 22:35:27 +0300

    add lab-4

--- app/server.js
+++ app/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello from Dockerized Node.js App!');
+    res.send('Hello, Docker Multi-Stage!');
 });
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
 
